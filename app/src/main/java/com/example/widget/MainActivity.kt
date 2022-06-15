021d40bbec11b6a5d0d169eac19a2ff991036325
Rudi <rudimulyadi01107@gmail.com>
2022-06-15 17:14:55 +0700

    widget APP

---
+++ app/src/main/java/com/example/widget/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.widget
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.widget.databinding.ActivityMainBinding
+
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        val getData = intent.getParcelableExtra<ModelLogin>("data")
+        binding.txtUserName.text = getData?.username.toString()
+        binding.txtPassword.text = getData?.password.toString()
+
+        binding.btnWidget.setOnClickListener {
+            val intent = Intent (this, Widget::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnIntentExplicit.setOnClickListener {
+            val intent = Intent(this, ImplicitIntent::class.java)
+            startActivity(intent)
+        }
+    }
+}
